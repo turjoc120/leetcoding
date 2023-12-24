273b4b6754243849b75df29ebe62846e8bc452b0
Turjo Chowdhury <86681794+turjoc120@users.noreply.github.com>
2023-12-24 23:53:50 +0600

    Time: 10 ms (71.95%) | Memory: 13.7 MB (98.11%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,6 +3,12 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int row=matrix.size(), col= matrix[0].size(), col0=1;
 
+        //skip the first col coz we have seprate conditon for it, otherwise it will colide check bellow in 3rd row first item is 0 it will mark m[0][0]'th item which means we are marking the row thats wrong 
+        /*
+        [1,2,3,4],
+        [5,0,7,8],
+        [0,1,1,1],
+        [1,1,1,0]*/
         for(int i=0; i<row; i++){
             if(matrix[i][0] == 0) col0=0;
             for(int j=1; j < col; j++){
@@ -13,6 +19,15 @@ public:
             }
         }
 
+
+//doing it backward so it wont affect the down items, skip the first col coz we are using it as a flag for marking rows and have a seprate variable for first col
+/*
+[x,-1,6,-7,0],
+[x,6,-8,-6,0],
+[1,2,-9,-6,-10]
+
+two 0's marks the rows and cols but when coming backward if we dont skip the first col at [2][0]'th item it will see the [i][0]'th flag is marked but meanwhile its acutally marked for rows col0 is handling the first col seprately so skip the first col j>0 ! j>=0
+*/
         for(int i=row-1; i>=0; i--){
             for(int j=col-1; j>0; j--){
                 if(matrix[0][j] ==0|| matrix[i][0]==0) matrix[i][j]=0;
