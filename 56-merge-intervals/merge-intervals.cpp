ffc8a7fb2e4bfe4af56b5802d1033b33297b6d17
Turjo Chowdhury <86681794+turjoc120@users.noreply.github.com>
2023-12-27 03:51:40 +0600

    Time: 36 ms (28.09%) | Memory: 20.4 MB (25.79%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -3,13 +3,12 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
         sort(intervals.begin(), intervals.end());
-        int start=INT_MIN, end=INT_MIN;
+        int end=INT_MIN;
 
         for(auto i: intervals){
             if(i[0] > end){
-                start=i[0];
                 end=i[1];
-                ans.push_back({start,end});
+                ans.push_back({i[0],end});
             }else{
                 end=max(end, i[1]);
                 ans.back()[1]=end;
