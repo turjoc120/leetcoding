cf08769f6ab1b7f71f5957a422a32b3127cf13da
Turjo Chowdhury <86681794+turjoc120@users.noreply.github.com>
2023-12-28 03:06:39 +0600

    Time: 6 ms (29.44%) | Memory: 9.4 MB (76.89%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -6,7 +6,7 @@ public:
         int i=m-1, j=n-1, k=(m+n)-1;
         while( j>=0){
             //check i>=0, if nums2 has more item than nums1 i will be less than 0 it will throw error, check if i>=0  
-            if(i>=0 && nums1[i] >= nums2[j]){
+            if(i>=0 && nums1[i] > nums2[j]){
                 nums1[k--]=nums1[i--];
             }else{
                 nums1[k--]=nums2[j--];
