c85f6e7e3005f7b7b2aae1cbace96dd5fe59608e
Turjo Chowdhury <86681794+turjoc120@users.noreply.github.com>
2024-01-27 20:42:29 +0600

    Time: 7 ms (88.53%) | Memory: 18.2 MB (13.12%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int majority1, majority2, count1=0, count2=0;
+        int majority1, majority2, count1=0, count2 = 0;
 
         for(auto i: nums){
-            if(count1 == 0 && i != majority2){
-                majority1=i;
-                count1=1;
-            }
-            else if(count2 == 0 && i != majority1){
-                majority2=i;
-                count2=1;
-            }
+            if(count1 == 0 && i != majority2) count1=1, majority1=i;
+            else if(count2 == 0 && i != majority1) count2=1, majority2=i;
             else if(i == majority1) count1++;
             else if(i == majority2) count2++;
-            else{
-                count1--, count2--;
-            }
+            else count1--, count2--;
         }
 
         count1=0, count2=0;
         for(auto i: nums){
-            if(i==majority1) count1++;
-            else if(i==majority2) count2++;
+            if(i == majority1) count1++;
+            else if(i == majority2) count2++;
         }
 
         vector<int>ans;
         if(count1 > (nums.size()/3)) ans.push_back(majority1);
         if(count2 > (nums.size()/3)) ans.push_back(majority2);
+
         return ans;
-        
     }
 };
