94f8d0b4382c170a39a070fc6f3e7eaf6746ed2e
Turjo Chowdhury <86681794+turjoc120@users.noreply.github.com>
2024-03-08 06:48:09 +0600

    Time: 3 ms (95.45%) | Memory: 19.9 MB (69.50%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.cpp
+++ 1008-binary-tree-cameras/binary-tree-cameras.cpp
@@ -12,20 +12,21 @@
 class Solution {
 public:
     int helper(TreeNode* root, int &ans){
-        if(root == NULL) return 1;
-        int left = helper(root->left,ans);
-        int right = helper(root->right,ans);
-        if(left == -1 || right == -1){
-            ans++;
-            return 0;
-        }
-        if(left == 0 || right == 0) return 1;
-        return -1;
-    }
+        if(!root) return 1;
 
+        int left = helper(root->left, ans);
+        int right = helper(root->right, ans);
+        
+        if(left == 0 || right == 0){
+            ans++;
+            return 2;
+        }
+        else if(left == 2 || right == 2) return 1;
+        return 0;
+    }
     int minCameraCover(TreeNode* root) {
         int ans = 0;
-        if(helper(root, ans) == -1) ans++;
+        if(helper(root, ans) == 0) ans++;
         return ans;
     }
 };
