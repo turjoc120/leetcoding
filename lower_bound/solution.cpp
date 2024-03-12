bf0dc688a29466cce39d949325f85b571e61150e
TURJO <turjoc120@gmail.com>
2024-03-13 05:34:55 +0600

    lower bound custom add

---
+++ lower_bound/solution.cpp
@@ -0,0 +1,13 @@
+int lowerBound(vector<int> arr, int n, int x)
+{
+    int start = 0, end = n - 1;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] >= x)
+            end = mid - 1;
+        else
+            start = mid + 1;
+    }
+    return start;
+}
