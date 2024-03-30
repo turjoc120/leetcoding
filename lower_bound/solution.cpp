356a3c0105a9c3cbb37244dea3e4940ad99aa98f
TURJO <turjoc120@gmail.com>
2024-03-30 23:18:11 +0600

    revisit

--- lower_bound/solution.cpp
+++ lower_bound/solution.cpp
@@ -77,4 +77,35 @@ bool search(vector<int> &nums, int target)
     return false;
 }
 
+bool search(vector<int> &nums, int target)
+{
+    int start = 0, end = nums.size() - 1;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target)
+            return true;
+        if (nums[start] == nums[mid] && nums[mid] == nums[end])
+            start++, end--;
+
+        else if (nums[start] <= nums[mid])
+        {
+            if (nums[start] <= target && target < nums[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        else
+        {
+            if (nums[mid] < target && target <= nums[end])
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+    }
+
+    return false;
+}
+
 // revisit done
