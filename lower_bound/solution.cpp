064340cb4bfb11060b2d739f4595ae9232a06fc4
TURJO <turjoc120@gmail.com>
2024-04-01 02:31:29 +0600

    sqrt

--- lower_bound/solution.cpp
+++ lower_bound/solution.cpp
@@ -108,4 +108,21 @@ bool search(vector<int> &nums, int target)
     return false;
 }
 
+/////////////////
+// >>>>>>>>>>>>floor squre root of a number
+int floorSqrt(int n)
+{
+    int start = 1, end = n;
+
+    while (start <= end)
+    {
+        long long mid = (start + end) / 2;
+        if (mid * mid <= n)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+    return end;
+}
+
 // revisit done
