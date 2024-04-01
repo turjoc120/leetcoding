fd2a97b672df9eb13e7c5b473c2f1f80fe8e5f9c
TURJO <turjoc120@gmail.com>
2024-04-01 06:08:05 +0600

    nth root

--- lower_bound/solution.cpp
+++ lower_bound/solution.cpp
@@ -126,3 +126,35 @@ int floorSqrt(int n)
 }
 
 // revisit done
+int pow(int mid, int n, int m)
+{
+    long long ans = 1;
+    for (int i = 0; i < n; i++)
+    {
+        ans *= mid;
+        if (ans > m)
+            return 2;
+    }
+    if (ans == m)
+        return 1;
+    return 0;
+}
+
+// get nth root
+int NthRoot(int n, int m)
+{
+    int start = 1, end = m;
+
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+        long long val = pow(mid, n, m);
+        if (val == 1)
+            return mid;
+        else if (val == 0)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+    return -1;
+}
