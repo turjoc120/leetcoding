1f950b3da26d2439bd7edbd273b1430c997f9d2c
Turjo Chowdhury <86681794+turjoc120@users.noreply.github.com>
2024-04-26 03:19:36 +0600

    Time: 118 ms (39.48%) | Memory: 121.3 MB (11.45%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    bool feasible(vector<int>& bloomDay, int k, int m, int mid){
-        int days = 0, adjecent=0;
+    bool feasible(vector<int> bloomDay, int mid, int m, int k){
+        int bouque = 0, flowers=0;
         for(auto i: bloomDay){
-            if(i <= mid) adjecent++;
-            else{
-                days+= adjecent / k;
-                adjecent = 0;
+            if(i <= mid) flowers++;
+            else flowers = 0;
+            if(flowers == k) {
+                flowers = 0;
+                bouque++;
             }
+            if(bouque == m) break;
         }
-        days+= adjecent / k;
-        return days >= m;
+        return bouque == m;
     }
-    
+
     int minDays(vector<int>& bloomDay, int m, int k) {
-        long long initDays = (long long)m * (long long)k, n = bloomDay.size();
+       long long initDays = (long long)m * (long long)k, n = bloomDay.size();
         if(initDays > n) return -1;
-        int start = INT_MAX, end= -1;
+
+        int start = INT_MAX, end = INT_MIN;
 
         for(auto i: bloomDay){
-            start = min(i, start);
-            end = max(i, end);
+            start = min(start, i);
+            end = max(end, i);
         }
 
         while(start <= end){
-            int mid = (start + end) / 2;
-            if(feasible(bloomDay, k, m, mid)) end = mid - 1;
+            int mid = start + (end - start) / 2;
+            if(feasible(bloomDay, mid, m, k)) end = mid - 1;
             else start = mid + 1;
         }
+
         return start;
     }
 };
