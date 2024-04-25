198111358a85b2592acfbddffbc1d2c27c8b5d8e
Turjo Chowdhury <86681794+turjoc120@users.noreply.github.com>
2024-04-26 04:45:03 +0600

    Time: 32 ms (90.72%) | Memory: 58.7 MB (5.51%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     bool feasible(vector<int> weights, int mid, int days){
-        int possDays = 0, weightSum = 0;
+        int sum = 0, myDays = 0;
         for(auto i: weights){
-            if(weightSum + i > mid){
-                possDays++;
-                weightSum = i;
-            } 
-            else weightSum += i;
+            if(sum + i > mid){
+                myDays++;
+                sum = i;
+            }  
+            else sum+=i;
         }
-        if(weightSum != 0) possDays++;
-        return possDays <= days;
+        myDays++;
+        return myDays <= days;
     }
 
     int shipWithinDays(vector<int>& weights, int days) {
-        int start = INT_MIN, end=0;
-
+        int start = INT_MIN, end = 0;
         for(auto i: weights){
             start = max(start, i);
             end +=i;
         }
 
         while(start <= end){
-            int mid = (start + end) / 2;
+            int mid = start + (end - start) / 2;
             if(feasible(weights, mid, days)) end = mid - 1;
             else start = mid + 1;
         }
-
         return start;
     }
 };
